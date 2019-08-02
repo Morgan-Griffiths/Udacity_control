191d3ae46bc3cab5ddfb05bc3d153ba86f70f5d8
Morgan <C5ipo7i@yahoo.com>
2019-08-01 23:49:20 -0700

    feat: modified plot to account for the type of data passed into it

--- plot.py
+++ plot.py
@@ -3,10 +3,15 @@ import math
 import matplotlib.pyplot as plt
 
 def plot(name,scores,interval = 1):
-    rolling_mean = [np.mean(scores[(slice_*interval):(slice_+1)*interval]) for slice_ in range(math.ceil(len(scores)/interval))]
-    x_axis = np.arange(len(rolling_mean)) * interval
-    plt.plot(x_axis, rolling_mean)
+    # rolling_mean = [np.mean(scores[(slice_*interval):(slice_+1)*interval]) for slice_ in range(math.ceil(len(scores)/interval))]
+    # x_axis = np.arange(len(rolling_mean)) * interval
+    # plt.plot(x_axis, rolling_mean)
+    plt.plot(scores)
     plt.ylabel('Score')
     plt.xlabel('Episode #')
     # plt.show()
     plt.savefig(str(name) +'_performance.png',bbox_inches='tight')
+
+if __name__ == "__main__":
+    x = np.linspace(0,100,50)
+    plot('test',x)
