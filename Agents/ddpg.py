1c588ca7a87cf04f25760d71dfaed142380d49d6
Morgan <C5ipo7i@yahoo.com>
2019-08-04 20:45:29 -0700

    feat: updated DDPG with future returns

--- Agents/ddpg.py
+++ Agents/ddpg.py
@@ -4,40 +4,51 @@ import torch
 import torch.nn.functional as F
 import os
 
-# print(os.getcwd())
+print(os.getcwd())
 from Buffers.replay_buffer import ReplayBuffer
 from Networks.models import Critic,Actor,hard_update
 import torch.optim as optim
 
 class DDPG(object):
-    def __init__(self,nA,nS,BUFFER_SIZE,min_buffer_size,batch_size,seed,L2,TAU,gamma=1.0,n_step=0.95):
+    def __init__(self,seed,nA,nS,config):
         self.seed = seed
         self.nA = nA
         self.nS = nS
-        self.Buffer_size = BUFFER_SIZE
-        self.min_buffer_size = min_buffer_size
-        self.batch_size = batch_size
-        self.L2 = L2
-        self.tau = TAU
-        self.gamma = gamma
-        self.n_step = n_step
+        self.buffer_size = config.buffer_size
+        self.min_buffer_size = config.min_buffer_size
+        self.batch_size = config.batch_size
+        self.L2 = config.L2
+        self.tau = config.TAU
+        self.gamma = config.gamma
+        self.gae_lambda = config.gae_lambda
+        self.num_agents= config.num_agents
         
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
-        self.R = ReplayBuffer(nA,BUFFER_SIZE,batch_size,seed)
+        self.R = ReplayBuffer(nA,self.buffer_size,self.batch_size,seed)
         self.local_critic = Critic(seed,nS,nA).to(self.device)
         self.target_critic = Critic(seed,nS,nA).to(self.device)
         self.local_actor = Actor(seed,nS,nA).to(self.device)
         self.target_actor = Actor(seed,nS,nA).to(self.device)
-        self.critic_optimizer = optim.Adam(self.local_critic.parameters(), lr = 1e-3,weight_decay=L2)
+        self.critic_optimizer = optim.Adam(self.local_critic.parameters(), lr = 1e-3,weight_decay=self.L2)
         self.actor_optimizer = optim.Adam(self.local_actor.parameters(), lr = 1e-4)
         
         # Copy the weights from local to target
         hard_update(self.local_critic,self.target_critic)
         hard_update(self.local_actor,self.target_actor)
 
-    def add(self,state,action,reward,next_state,done):
-        self.R.add(state,action,reward,next_state,done)
+    def load_weights(self,path):
+        self.policy.load_state_dict(torch.load(path))
+        self.policy.eval()
+
+    def save_weights(self,path):
+        directory = os.path.dirname(path)
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+        torch.save(self.policy.state_dict(), path)
+
+    def add(self,trajectory):
+        self.R.add(trajectory)
         
     def step(self):
         # Sample memory if len > minimum
@@ -52,37 +63,63 @@ class DDPG(object):
         states,actions,rewards,next_states,dones = samples
 
         target_actions = self.target_actor(next_states)
-        Q_targets = self.target_critic(next_states,target_actions)
-        target_y = rewards + (self.gamma*Q_targets*(1-dones))
+        next_values = self.target_critic(next_states,target_actions).squeeze(-1)
+        values = self.local_critic(states,actions)
     
         # GAE rewards
-        # GAE_rewards = torch.tensor(self.GAE(rewards.cpu().numpy()))
-        # target_y = GAE_rewards + (self.gamma*Q_targets*(1-dones))
+        gae_r,future_r = self.GAE_rewards(rewards,values,next_values,dones)
+        # Reshape
+        # next_values = next_values.view(self.batch_size*self.num_agents,1)
+        # dones = dones.view(self.batch_size*self.num_agents,1)
+        # states = states.view(self.batch_size*self.num_agents,1)
+        # actions = actions.vie
+
+        target_y = future_r + (self.gamma*next_values*(1-dones))
+        current_y = self.local_critic(states,actions).squeeze(-1)
 
         # update critic
         self.critic_optimizer.zero_grad()
-        current_y = self.local_critic(states,actions)
         critic_loss = (target_y - current_y).mean()**2
         critic_loss.backward()
-        torch.nn.utils.clip_grad_norm(self.local_critic.parameters(),1)
+        torch.nn.utils.clip_grad_norm_(self.local_critic.parameters(),1)
         self.critic_optimizer.step()
 
         # update actor
         self.actor_optimizer.zero_grad()
         local_actions = self.local_actor(states)
-        actor_loss = self.local_critic(states, local_actions)
-        actor_loss = -actor_loss.mean()
+        actor_loss = -self.local_critic(states, local_actions).mean()
         actor_loss.backward()
         self.actor_optimizer.step()
-        # if sum(rewards) > 0:
-        #     print('finally')
         
-    def GAE(self,rewards):
-        """
-        Generalized Advantage Estimate.
-        N_step discounted returns
-        """
-        return np.sum([sum(rewards[:i+1])*((1-self.n_step)*self.n_step**i) for i in range(rewards.shape[0])])
+    def GAE_rewards(self,rewards,values,next_values,dones):
+        # Return GAE and future rewards
+        N = rewards.shape[0]
+        A = self.num_agents
+        # Get values and next_values in the same shape to quickly calculate TD_errors
+        combined = self.gamma * self.gae_lambda
+
+        # rewards = np.concatenate(rewards).reshape(N,A)
+        next_values = np.concatenate(next_values.cpu().detach().numpy()).reshape(N,A)
+        values = np.concatenate(values.cpu().detach().numpy()).reshape(N,A)
+        TD_errors = rewards + next_values - values
+        advs = np.zeros(rewards.shape)
+        returns = np.zeros(rewards.shape)
+        # For returns
+        discounted_gamma = self.gamma**np.arange(N)
+        j = 1
+        for index in reversed(range(N)):
+            P = N-index
+            discounts = combined ** np.arange(0,N-index)
+            returns[index,:] = np.sum(rewards[index:,:] * np.repeat([discounted_gamma[:j]],A,axis=1).reshape(P,A),axis=0)
+            advs[index,:] = np.sum(TD_errors[index:,:] * np.repeat([discounts],A,axis=1).reshape(P,A),axis=0)
+            j += 1
+        # Normalize and reshape
+        # returns = torch.from_numpy(returns.reshape(N*A,1)).float().to(self.device)
+        # advs = torch.from_numpy(advs.reshape(N*A,1)).float().to(self.device)
+        returns = torch.from_numpy(returns).float().to(self.device)
+        advs = torch.from_numpy(advs).float().to(self.device)
+        advs = (advs - advs.mean()) / advs.std()
+        return advs,returns
          
     def act(self,state,N):
         state = torch.from_numpy(state).float().to(self.device)
